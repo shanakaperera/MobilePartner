082b79368895b1a1e76c4655b508d2f49f569e5e
shanakaperera <shanakaperera08@gmail.com>
2016-11-24 20:19:34 +0530

    2016-11-24 final

--- src/db_ddl/CreateDB.java
+++ src/db_ddl/CreateDB.java
@@ -5,14 +5,9 @@
  */
 package db_ddl;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Properties;
 
 /**
